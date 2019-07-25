3b5a575fd99e407dc2a3d203ddf03f6f92f96f1b
Isaac Wasserman <isaac@Isaacs-MacBook-Air.local>
2019-07-25 01:09:20 -0400

    tried something

--- netlify-functions/submission-created.js
+++ netlify-functions/submission-created.js
@@ -1,29 +1,17 @@
 const SG_KEY = process.env.SENDGRID_API_KEY
 
-const helper = require('sendgrid').mail
+const client = require("@sendgrid/mail");
 
-exports.handler = (event, context, callback) => {
-	let submissionBody = JSON.parse(event.body).payload
+exports.handler = function(event, context, callback) {
+  client.setApiKey(process.env.SENDGRID_API_KEY);
 
-  let from_email = new helper.Email(payload.data.email);
-  let to_email = new helper.Email('isaacrwasserman@gmail.com');
+  let data = JSON.parse(event.body).payload.data;
 
-  let subject = payload.data.name + " sent you a message";
-
-  let content = subject + ': \n \n' + payload.data.message + '\n \n Reply to: ' + payload.data.email
-  let mail = new helper.Mail(from_email, subject, to_email, content)
-
-  let sg = require('sendgrid')(SG_KEY)
-
-  let request = sg.emptyRequest({
-    method: 'POST',
-		path: '/v3/mail/send',
-		body: mail.toJSON()
-  })
-
-  sg.API(request, function(error, response){
-    if(error){
-      console.log(error.response.body)
-    }
-  })
-}
+  const msg = {
+    to: 'isaacrwasserman@gmail.com',
+    from: data.email,
+    subject: 'New message from ' + data.name,
+    text: data.message,
+  };
+  client.send(msg);
+};
